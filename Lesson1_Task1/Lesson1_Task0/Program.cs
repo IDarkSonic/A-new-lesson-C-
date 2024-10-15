b97ab40967060ce32d1e7bf94506d9ff53d26abd
Vladislav <666darksite666@mail.ru>
2024-10-15 18:47:38 +0300

    add options Lesson1_Task0

---
+++ Lesson1_Task1/Lesson1_Task0/Program.cs
@@ -0,0 +1,12 @@
+﻿// Console.WriteLine("Hello World");
+
+
+int Value = 100;
+Console.WriteLine(Value);
+
+Double d = 5.55;
+char c = 'i';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
